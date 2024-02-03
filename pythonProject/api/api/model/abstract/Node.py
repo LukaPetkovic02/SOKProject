ef9bb7d111ca375f406eed5a69b997b239737968
StefanDjuricaSV35-2021 <stefandjurica420@gmail.com>
2024-02-03 17:38:17 +0100

    Added a graph model and an abstract edge and node model

---
+++ pythonProject/api/api/model/abstract/Node.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from uuid import uuid4
+
+
+class Node(ABC):
+
+    @abstractmethod
+    def __init__(self, **kwargs):
+        super(Node, self).__init__()
+        self._id = uuid4()
+        self._name = kwargs.get("name", None)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "data": self.get_data()
+        }
+
+    @abstractmethod
+    def get_data(self):
+        pass
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self._id == other.id
+        return False
+
+
