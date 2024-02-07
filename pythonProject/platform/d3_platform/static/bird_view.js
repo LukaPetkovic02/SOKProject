9523adadaa9da270399cd855accb9b4f035c1bde
Borislav Čelar <bora.celar@gmail.com>
2024-02-07 10:34:03 +0100

    #18 completed bird view

--- pythonProject/platform/d3_platform/static/bird_view.js
+++ pythonProject/platform/d3_platform/static/bird_view.js
@@ -19,8 +19,24 @@ window.onload = function() {
 
         copiedSVG.setAttribute('viewBox', offsetX+' '+offsetY+' '+3000+' 3000');
 
+
         // Remove the ID attribute to prevent duplicate IDs
         copiedSVG.removeAttribute('id');
+
+
+        var viewBoxParams = originalSVG.viewBox.baseVal;
+        var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        // Set attributes for the rectangle
+        rect.setAttribute('x', viewBoxParams.x);
+        rect.setAttribute('y', viewBoxParams.y);
+        rect.setAttribute('width', viewBoxParams.width);
+        rect.setAttribute('height', viewBoxParams.height);
+        rect.setAttribute('fill', 'none');
+        rect.setAttribute('stroke', 'red'); // Set stroke color
+        rect.setAttribute('stroke-width', '20'); // Set stroke width
+
+        // Append the rectangle to the copied SVG
+        copiedSVG.appendChild(rect);
 
         // Remove any existing SVG content in the copied container
         copiedSVGContainer.innerHTML = '';
